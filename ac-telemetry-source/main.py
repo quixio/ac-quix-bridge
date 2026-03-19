1273ebca2168f2df621b7a91f76d85599bd6f0d1
Daniel Lastic <daniel@quix.io>
2026-03-19 16:38:55 +0100

    Refactor main function to initialize application and source with environment variables

--- ac-telemetry-source/main.py
+++ ac-telemetry-source/main.py
@@ -2,10 +2,18 @@ from quixstreams import Application
 from ac_source import AssettoCorsaSource
 import os
 
-app = Application()
-source = AssettoCorsaSource(name="ac-telemetry-source")
-sdf = app.dataframe(source=source)
-sdf = sdf.print()
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def main():
+    app = Application(consumer_group="ac_telemetry_source", auto_create_topics=True)
+    source = AssettoCorsaSource(name="ac-telemetry-source")
+    output_topic = app.topic(name=os.environ["output"])
+
+    app.add_source(source=source, topic=output_topic)
+    app.run()
+
 
 if __name__ == "__main__":
-    app.run()
+    main()
