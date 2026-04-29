e1b8dc6b419d102ead3a20c31b6e5957fcc7480f
Daniel Lastic <daniel@quix.io>
2026-04-29 14:43:24 +0200

    Replace vitest mock absolute path with portable specifier

--- telemetry-comparison/vitest.setup.js
+++ telemetry-comparison/vitest.setup.js
@@ -1,6 +1,3 @@
 import { vi } from 'vitest';
 
-vi.mock(
-  '/Users/daniel/repos/ac-quix-bridge/telemetry-comparison/static/modules/markdown.js',
-  () => ({ renderMarkdown: (t) => t || '' }),
-);
+vi.mock('./static/modules/markdown.js', () => ({ renderMarkdown: (t) => t || '' }));
