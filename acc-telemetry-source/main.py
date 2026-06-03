48977144a1632ebedee539020de3de0abbabb25f
Daniel Lastic <daniel@quix.io>
2026-06-03 11:41:27 +0200

    feat(acc-telemetry-source): switch to direct-broker connection (Mode B)

--- acc-telemetry-source/main.py
+++ acc-telemetry-source/main.py
@@ -3,6 +3,7 @@ import os
 
 from quixstreams import Application
 from acc_source import AssettoCorsaCompetizioneSource, configure_logging
+from quixstreams.kafka import ConnectionConfig
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,8 +12,15 @@ logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO").upper())
 configure_logging()
 
 
+connection = ConnectionConfig(
+    bootstrap_servers=os.environ["Quix__Broker__Address"],
+    security_protocol="ssl",
+    enable_ssl_certificate_verification=False,
+    ssl_endpoint_identification_algorithm="none",
+)
+
 def main():
-    app = Application(consumer_group="acc_telemetry_source", auto_create_topics=True)
+    app = Application(consumer_group="acc_telemetry_source", auto_create_topics=True, broker_address=connection)
     output_topic = app.topic(name=os.environ["output"])
     session_topic = app.topic(name=os.environ.get("session_output", "acc-telemetry-session"))
 
