c6c294a0162c661643418b70a5d3c2332d7924d5
tomas-quix <78492422+tomas-quix@users.noreply.github.com>
2026-06-19 17:34:01 +0200

    Cleanup (#14)
    * Delete deployment leaderboard-service
    
    * Update deployment BestLap cache
    
    * Deleted application files from leaderboard-service

--- leaderboard-service/ui/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LeaderboardTab } from "@/components/leaderboard-tab"
-
-export default function Page() {
-  return (
-    <main className="min-h-screen p-4">
-      <LeaderboardTab />
-    </main>
-  )
-}
