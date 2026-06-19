796122e10a6baeaee3518cbb08052f5d8fa7bec2
Tomas Neubauer <tomas@quix.ai>
2026-06-19 10:09:19 +0000

    Update file quixlab/main.py

--- quixlab/main.py
+++ quixlab/main.py
@@ -1,53 +1,56 @@
 import quixlab as ql
 
-canvas = ql.Canvas(title="My Notebook", lake_tree_open=['ac_telemetry', 'ac_telemetry/environment=prague_office', 'ac_telemetry/environment=prague_office/test_rig=fanatec_csl_dd', 'ac_telemetry/environment=prague_office/test_rig=fanatec_csl_dd/experiment=TestDrive', 'ac_telemetry/environment=prague_office/test_rig=fanatec_csl_dd/experiment=TestDrive/driver=patrick', 'ac_telemetry/environment=prague_office/test_rig=fanatec_csl_dd/experiment=TestDrive/driver=patrick/track=Spa', 'ac_telemetry/environment=prague_office/test_rig=fanatec_csl_dd/experiment=TestDrive/driver=patrick/track=Spa/carModel=lamborghini_huracan_gt3_evo', 'ac_telemetry/environment=prague_office/test_rig=fanatec_csl_dd/experiment=TestDrive/driver=patrick/track=Spa/carModel=lamborghini_huracan_gt3_evo/session_id=2026-06-03T14:11:08.847Z', 'ac_telemetry/environment=prague_office/test_rig=fanatec_csl_dd/experiment=TestDrive/driver=patrick/track=Spa/carModel=lamborghini_huracan_gt3_evo/session_id=2026-06-03T11:39:27.026Z'])
+canvas = ql.Canvas(title="My Notebook", lake_tree_open=['ac_telemetry_prod', 'ac_telemetry_prod/environment=prague_office', 'ac_telemetry_prod/environment=prague_office/test_rig=fanatec_csl_dd'])
 
 
-@canvas.dataset(position=(-61, 151), size=(902, 561), code_height=200)
-def ac_telemetry():
+@canvas.dataset(position=(420, 77), size=(839, 477), code_height=200)
+def ac_telemetry_prod():
     return ql.sql("""SELECT *
-    FROM ac_telemetry
+    FROM ac_telemetry_prod
     WHERE environment = 'prague_office'
       AND test_rig = 'fanatec_csl_dd'
-      AND experiment = 'TestDrive'
-      AND driver = 'tomas'
-      AND track = 'Spa'
-      AND carModel = 'lamborghini_huracan_gt3_evo'
-      AND session_id = '2026-06-04T13:01:02.619Z'
-    ORDER BY timestamp_ms""")
+      AND experiment = 'tyre_pressure'
+      AND driver = 'tomas neubauer'
+    """)
 
 
-@canvas.cell(position=(1089, 104), size=(831, 578), code_height=200, viz={'type': 'line', 'x': 'timestamp_ms', 'y': ['speedKmh']})
-def cell_1(ac_telemetry):
-    import pandas as pd
-
-    cols = {}
-    for lap, g in ac_telemetry.groupby("lap", sort=True):
-        g = g.sort_values("timestamp_ms")
-        offset = g["timestamp_ms"] - g["timestamp_ms"].iloc[0]   # zeroed, in ms
-        s = pd.Series(g["speedKmh"].values, index=offset.values,
-                      name=f"Speed_lap_{lap}")
-        cols[lap] = s
-
-    result = pd.concat(cols.values(), axis=1)
-    result.index.name = "elapsed_ms"
-
-    cols = {}
-    for lap, g in ac_telemetry.groupby("lap", sort=True):
-        g = g.sort_values("timestamp_ms")
-        offset = g["timestamp_ms"] - g["timestamp_ms"].iloc[0]
-        s = pd.Series(g["speedKmh"].values, index=offset.values, name=f"Speed_lap_{lap}")
-        cols[lap] = s
-
-    result = pd.concat(cols.values(), axis=1)
-    result.index.name = "elapsed_ms"
-    result = result.ffill()   
-    result = result.reset_index()
-    result
+@canvas.dataset(position=(-2185, -641), size=(844, 613), code_height=230)
+def car_telemetry():
+    return ql.sql("""SELECT date, ts_ms, speed, rpm, n_gear
+    FROM car_telemetry
+    WHERE year = 2023
+      AND circuit = 'Monza'
+      AND session_type = 'Race'
+      AND session_name = 'Race'
+      AND driver_acronym = 'HAM'
+      AND lap_number = 12
+    ORDER BY ts_ms
+    """)
 
 
-@canvas.cell(position=(-714, 194), size=(560, 420), code_height=200)
-def cell_2():
+@canvas.cell(position=(-1179, -641), size=(937, 630), code_height=200, viz={'type': 'line', 'x': 'ts_ms', 'y': ['speed']})
+def cell_1(car_telemetry):
+    return [
+        ql.ui.markdown(r"""
+    # Channel explorer
+
+    Some **markdown** text.
+    """),
+        car_telemetry]
+
+
+@canvas.stream(position=(337, 709), size=(563, 512), code_height=232)
+def stream_1():
+    return ql.topic('', limit=200000)
+
+
+@canvas.cell(position=(957, 709), size=(660, 546), code_height=200)
+def cell_2(stream_1):
+    df = stream_1.df
+    df["lap"] = df["completedLaps"] + 1
+
+    stats_df = df.groupby("lap").agg(["min", "max"])
+    return stats_df.reset_index()
 
 
 if __name__ == "__main__":
